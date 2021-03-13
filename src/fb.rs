cbb6e5293686b476f5e9961e708ec2f4f9f16d9d
Ashish Rao <aprao@stanford.edu>
2021-03-13 11:40:48 -0800

    global config in fb

--- src/fb.rs
+++ src/fb.rs
@@ -1,8 +1,5 @@
 #[path = "mailbox.rs"] mod mailbox;
 
-extern crate lazy_static;
-use lazy_static::lazy_static;
-
 #[repr(C, align(16))]
 struct FbConfigT {
     width: u32,
@@ -19,40 +16,26 @@ struct FbConfigT {
 
 const GPIO_BASE: u32 = 0x20200000;
 
-lazy_static! {
-    static ref fb_width: u32 = 100;
-    static ref fb_height: u32 = 100;
-    static ref fb_virtual_height: u32 = 100;
-    static ref fb_virtual_width: u32 = 100;
-    static ref fb_pitch: u32 = 0;
-    static ref fb_bit_depth: u32 = 32;
-    static ref fb_x_offset: u32 = 0;
-    static ref fb_y_offset: u32 = 0;
-    static ref fb_framebuffer: u32 = 0;
-    static ref fb_total_bytes: u32 = 0;
-}
+static mut FB_CONFIG: FbConfigT = FbConfigT {
+    width: 100,
+    height: 100,
+    virtual_width: 100,
+    virtual_height: 100,
+    pitch: 0,
+    bit_depth: 32,
+    x_offset: 0,
+    y_offset: 0,
+    framebuffer: 0,
+    total_bytes: 0,
+};
+
+pub fn fb_init(u32 width, u32 height, u32 depth_in_bytes, )
 
 pub fn test() {
     let gpio = GPIO_BASE as *const u32;
     let led_on = unsafe { gpio.offset(8) as *mut u32 };
     
-    // I have no clue what the & is doing here
-    //unsafe { *fb_width };
-
-    let config = FbConfigT {
-        width: 100,
-        height: 100,
-        virtual_width: 100,
-        virtual_height: 100,
-        pitch: 0,
-        bit_depth: 32,
-        x_offset: 0,
-        y_offset: 0,
-        framebuffer: 0,
-        total_bytes: 0,
-    };
-    
-    let config_addr: u32 = (&config as *const _) as u32;
+    let config_addr: u32 = unsafe { (&FB_CONFIG as *const _) as u32 };
     let mailbox_success: bool = mailbox::mailbox_request(1, config_addr);
 
     if mailbox_success {
