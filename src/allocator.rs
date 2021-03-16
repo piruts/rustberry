c413470679d5e40d08f26b831a8ae5a2927e4691
Flynn Dreilinger <flynnd@stanford.edu>
2021-03-15 18:19:42 -0600

    add alloc test

--- src/allocator.rs
+++ src/allocator.rs
@@ -69,5 +69,11 @@ fn align_up(addr: usize, align: usize) -> usize {
 
 #[test_case]
 fn test_alloc() {
-    let x = Box::new(41);
+    let x = 5;
+    let y = &x;
+    let z = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    assert_eq!(5, *z);
 }
