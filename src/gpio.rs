6a994aa1f95eb31d3b5d53f764202a8f52f0fff6
Flynn Dreilinger <flynnd@stanford.edu>
2021-03-15 18:20:13 -0600

    move dev_barrier fn to cpu module

--- src/gpio.rs
+++ src/gpio.rs
@@ -1,24 +1,22 @@
+use crate::cpu;
+
 const GPIO_BASE: u32 = 0x20200000;
 const GPIO_FSEL0: *mut u32 = GPIO_BASE as *mut u32;
 const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
 const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
 const GPIO_LEV0: *mut u32 = (GPIO_BASE + 0x34) as *mut u32;
 
-extern "C" {
-    pub fn dev_barrier();
-}
-
 pub unsafe fn set_function(pin: isize, function: u32) {
-    dev_barrier();
+    cpu::dev_barrier();
     let fsel: *mut u32 = GPIO_FSEL0.offset(pin / 10);
     fsel.write_volatile(fsel.read_volatile() & !(0b111 << ((pin % 10) * 3)));
     fsel.write_volatile(fsel.read_volatile() | (function << ((pin % 10) * 3)));
-    dev_barrier();
+    cpu::dev_barrier();
 }
 
 #[allow(unused)]
 pub unsafe fn get_function(pin: isize) -> u32 {
-    dev_barrier();
+    cpu::dev_barrier();
     let fsel: *mut u32 = GPIO_FSEL0.offset(pin / 10);
     return (fsel.read_volatile() >> ((pin % 10) * 3)) & 0b111;
 }
@@ -35,7 +33,7 @@ pub unsafe fn set_output(pin: isize) {
 
 #[allow(unused)]
 pub unsafe fn write(pin: isize, value: u32) {
-    dev_barrier();
+    cpu::dev_barrier();
     if value == 0 {
         let clr: *mut u32 = GPIO_CLR0.offset(pin / 32);
         clr.write_volatile(1 << (pin % 32));
@@ -43,12 +41,12 @@ pub unsafe fn write(pin: isize, value: u32) {
         let set: *mut u32 = GPIO_SET0.offset(pin / 32);
         set.write_volatile(1 << (pin % 32));
     }
-    dev_barrier();
+    cpu::dev_barrier();
 }
 
 #[allow(unused)]
 pub unsafe fn read(pin: isize) -> u32 {
-    dev_barrier();
+    cpu::dev_barrier();
     let lev: *mut u32 = GPIO_LEV0.offset(pin / 32);
     return (lev.read_volatile() >> (pin % 32)) & 0b1;
 }
