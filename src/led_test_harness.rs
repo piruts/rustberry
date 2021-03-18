ad8307410f1b5e1d1eaa32e85882a057ac484ca9
Flynn Dreilinger <flynnd@stanford.edu>
2021-03-17 21:07:27 -0600

    blink faster

--- src/led_test_harness.rs
+++ src/led_test_harness.rs
@@ -13,11 +13,11 @@ pub fn success() -> ! {
         unsafe {
             *(led_on) = 1 << 15;
         }
-        cpu::sleep(1000000);
+        cpu::sleep(50000);
         unsafe {
             *(led_off) = 1 << 15;
         }
-        cpu::sleep(1000000);
+        cpu::sleep(50000);
     }
 }
 
